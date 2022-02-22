af10e1d74f4e5c44b08819dee5895a1968f1320e
unknown <megyko1234@gmail.com>
2022-02-22 09:29:53 +0100

    Vegleges megoldas

--- Tavolsag.java
+++ Tavolsag.java
@@ -22,17 +22,15 @@ System.out.println("Szoft I/N");
 
 Scanner sc = new Scanner(System.in);
 System.out.print("Magassag : ");
-double aoldal=sc.nextDouble();
+double magassag=sc.nextDouble();
+System.out.print("Epulet szoge");
+double szog =sc.nextDouble();
+
+double messze = magassag/(szog/180*Math.PI);
+
+System.out.printf("Tavolsag az epulettol : %.5f\n", messze);
 
 
-System.out.print("epulet szoge :");
-double szog=sc.nextDouble();
- 
 
-
-
-double a=Math.sin(aoldal* Math.PI/180);
-double d = Math.sin(Math.toRadians(a));
-System.out.println("Elottunk levo epulet tavolsaga: "+ d);
 }
 }
