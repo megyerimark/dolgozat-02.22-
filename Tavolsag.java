cebf17122e8c4e0e05043652a1bf2473afa3e094
unknown <megyko1234@gmail.com>
2022-02-22 08:47:46 +0100

    Kezdes

---
+++ Tavolsag.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+* File: ButtonPanel.java
+* Author:Megyeri Márk Máté 
+* Copyright: 2022, Megyeri Márk
+* Group: Szoft I/N
+* Date: 2022-02-22
+* Github: https://github.com/megyeri/
+* Licenc: GNU GPL
+*/
+ 
+
+
+class Tavolsag{
+public static void main(String[] args){
+System.out.println("Megyeri Mark Mate");
+System.out.println("2022.02.22");
+System.out.println("Szoft I/N");
+
+System.out.print("a oldal:");
+
+Scanner sc = new Scanner(System.in); 
+
+int aoldal=sc.nextInt();
+
+
+
+}
+}
